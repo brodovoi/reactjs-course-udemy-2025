d71144d5f61bbdb92547fc32715b189070b4c3ef
Artem Borodovoy <a.borodovoi@gmail.com>
2025-03-29 21:09:33 +0200

    an  updated

--- projects/01-projects-tasklist/src/App.jsx
+++ projects/01-projects-tasklist/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   const [openSection, setOpenSection] = useState({
     taskList: false,
     tasks: true,
@@ -14,6 +16,13 @@ function App() {
     }));
   }
 
+  function addTask(task) {
+    setTasks([...tasks, { ...task, completed: false, id: Date.now() }]);
+  }
+
+  const activeTasks = tasks.filter((task) => !task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
+
   return (
     <div className="app">
       <div className="task-container">
@@ -23,7 +32,7 @@ function App() {
           onClick={() => toggleSection('taskList')}>
           +
         </button>
-        {openSection.taskList && <TaskForm />}
+        {openSection.taskList && <TaskForm addTask={addTask} />}
       </div>
 
       <div className="task-container">
@@ -38,7 +47,7 @@ function App() {
           <button className={`sort-button `}>By Date</button>
           <button className={`sort-button `}>By Priority</button>
         </div>
-        {openSection.tasks && <TaskList />}
+        {openSection.tasks && <TaskList activeTasks={activeTasks} />}
       </div>
 
       <div className="completed-task-container">
@@ -56,44 +65,71 @@ function App() {
 }
 
 function TaskForm({ addTask }) {
+  const [title, setTitle] = useState('');
+  const [priority, setPriority] = useState('Low');
+  const [deadline, setDeadline] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (title.trim() && deadline) {
+      addTask({ title, priority, deadline });
+      setTitle('');
+      setPriority('Low');
+      setDeadline('');
+    }
+  }
+
   return (
-    <form action="" className="task-form">
-      <input type="text" value="" placeholder="Task title" required />
-      <select value="">
+    <form action="" className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        placeholder="Task title"
+        required
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
         <option value="Low">Low</option>
         <option value="Medium">Medium</option>
         <option value="High">High</option>
       </select>
-      <input type="datetime-local" required value="" />
+      <input
+        type="datetime-local"
+        required
+        value={deadline}
+        onChange={(e) => setDeadline(e.target.value)}
+      />
       <button type="submit">Add task</button>
     </form>
   );
 }
 
-function TaskList() {
+function TaskList({ activeTasks }) {
   return (
     <ul className="task-list">
-      <TaskItem />
+      {activeTasks.map((task) => (
+        <TaskItem task={task} key={task.id} />
+      ))}
     </ul>
   );
 }
 
 function CompletedTaskList() {
-  return (
-    <ul className="completed-task-list">
-      <TaskItem />
-    </ul>
-  );
+  return <ul className="completed-task-list">{/* <TaskItem /> */}</ul>;
 }
 
-function TaskItem() {
+function TaskItem({ task }) {
+  const { title, priority, deadline, id, completed } = task;
+
   return (
-    <li className={`task-item`}>
+    <li className={`task-item ${priority.toLowerCase()}`}>
       <div className="task-info">
         <div>
-          Title <strong>Medium</strong>
+          {title} <strong>{priority}</strong>
         </div>
-        <div className="task-deadline">Due: {new Date().toLocaleString()}</div>
+        <div className="task-deadline">
+          Due: {new Date(deadline).toLocaleString()}
+        </div>
       </div>
       <div className="task-buttons">
         <button className="complete-button">Complete</button>
